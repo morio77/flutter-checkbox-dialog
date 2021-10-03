ef3a3e0c89374c213bb2ff1458a7c2a6ce05d20c
morio77 <howtostudyy77@gmail.com>
2021-10-03 21:35:59 +0900

    ダイアログ上で採苗がされて、かつ、呼び出し元のonChangedも動くように

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '',
+      title: 'sample_flutter_checkbox_dialog',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -21,23 +21,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var checkboxValue = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('test'),
+        title: const Text('sample_flutter_checkbox_dialog'),
       ),
       body: Center(
         child: TextButton(
           onPressed: () async {
-            await FlutterCheckboxDialog().showCheckboxDialog(context);
+            await FlutterCheckboxDialog().showCheckboxDialog(
+              context,
+              checkboxValue,
+              const Text('次回から確認しない'),
+              _onChanged,
+              title: const Text('確認ダイアログ'),
+            );
           },
           child: const Text('ダイアログを開く'),
         ),
       ),
     );
+  }
+
+  void _onChanged(bool value) {
+    setState(() {
+      checkboxValue = value;
+    });
   }
 }
--- lib/flutter_checkbox_dialog.dart
+++ lib/flutter_checkbox_dialog.dart
@@ -1,38 +1,178 @@
 import 'package:flutter/material.dart';
 
 class FlutterCheckboxDialog {
-  Future<void> showCheckboxDialog(BuildContext context) async {
-    final result = await showDialog(
+  Future<void> showCheckboxDialog(
+    BuildContext context,
+    bool checkboxValue,
+    Widget checkboxTileTitle,
+    void Function(bool) onChanged, {
+    Key? key,
+    Widget? title,
+    EdgeInsetsGeometry? titlePadding,
+    TextStyle? titleTextStyle,
+    Widget? content,
+    EdgeInsetsGeometry contentPadding =
+        const EdgeInsets.fromLTRB(24.0, 20.0, 24.0, 24.0),
+    TextStyle? contentTextStyle,
+    List<Widget>? actions,
+    EdgeInsetsGeometry actionsPadding = EdgeInsets.zero,
+    MainAxisAlignment? actionsAlignment,
+    VerticalDirection? actionsOverflowDirection,
+    double? actionsOverflowButtonSpacing,
+    EdgeInsetsGeometry? buttonPadding,
+    Color? backgroundColor,
+    double? elevation,
+    String? semanticLabel,
+    EdgeInsets insetPadding =
+        const EdgeInsets.symmetric(horizontal: 40.0, vertical: 24.0),
+    Clip clipBehavior = Clip.none,
+    ShapeBorder? shape,
+    bool scrollable = false,
+  }) async {
+    await showDialog(
       barrierDismissible: false,
       context: context,
       builder: (_) {
-        return const _AlertDialog();
+        return _AlertDialog(
+          checkboxValue,
+          checkboxTileTitle,
+          onChanged,
+          key,
+          title,
+          titlePadding,
+          titleTextStyle,
+          content,
+          contentPadding,
+          contentTextStyle,
+          actions,
+          actionsPadding,
+          actionsAlignment,
+          actionsOverflowDirection,
+          actionsOverflowButtonSpacing,
+          buttonPadding,
+          backgroundColor,
+          elevation,
+          semanticLabel,
+          insetPadding,
+          clipBehavior,
+          shape,
+          scrollable,
+        );
       },
     );
   }
 }
 
 class _AlertDialog extends StatefulWidget {
-  const _AlertDialog({Key? key}) : super(key: key);
+  bool checkboxValue;
+  Widget checkboxTileTitle;
+  void Function(bool) onChanged;
+  final Key? key;
+  final Widget? title;
+  final EdgeInsetsGeometry? titlePadding;
+  final TextStyle? titleTextStyle;
+  final Widget? content;
+  final EdgeInsetsGeometry contentPadding;
+  final TextStyle? contentTextStyle;
+  final List<Widget>? actions;
+  final EdgeInsetsGeometry actionsPadding;
+  final MainAxisAlignment? actionsAlignment;
+  final VerticalDirection? actionsOverflowDirection;
+  final double? actionsOverflowButtonSpacing;
+  final EdgeInsetsGeometry? buttonPadding;
+  final Color? backgroundColor;
+  final double? elevation;
+  final String? semanticLabel;
+  final EdgeInsets insetPadding;
+  final Clip clipBehavior;
+  final ShapeBorder? shape;
+  final bool scrollable;
+  _AlertDialog(
+    this.checkboxValue,
+    this.checkboxTileTitle,
+    this.onChanged,
+    this.key,
+    this.title,
+    this.titlePadding,
+    this.titleTextStyle,
+    this.content,
+    this.contentPadding,
+    this.contentTextStyle,
+    this.actions,
+    this.actionsPadding,
+    this.actionsAlignment,
+    this.actionsOverflowDirection,
+    this.actionsOverflowButtonSpacing,
+    this.buttonPadding,
+    this.backgroundColor,
+    this.elevation,
+    this.semanticLabel,
+    this.insetPadding,
+    this.clipBehavior,
+    this.shape,
+    this.scrollable,
+  );
 
   @override
   _AlertDialogState createState() => _AlertDialogState();
 }
 
 class _AlertDialogState extends State<_AlertDialog> {
+  bool? _value;
+
+  @override
+  void initState() {
+    super.initState();
+    _value = widget.checkboxValue;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final _checkboxListTile = CheckboxListTile(
+      value: _value,
+      onChanged: (value) {
+        setState(() {
+          _value = value;
+        });
+        widget.onChanged(value!);
+      },
+      title: widget.checkboxTileTitle,
+      controlAffinity: ListTileControlAffinity.leading,
+    );
+
+    Widget _content;
+    if (widget.content == null) {
+      _content = _checkboxListTile;
+    } else {
+      _content = Row(
+        children: [
+          widget.content!,
+          _checkboxListTile,
+        ],
+      );
+    }
+
     return AlertDialog(
-      title: const Text('アラートダイアログ'),
-      content: const Text('アラートダイアログの中身'),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('閉じる'),
-        )
-      ],
+      key: widget.key,
+      title: widget.title,
+      titlePadding: widget.titlePadding,
+      titleTextStyle: widget.titleTextStyle,
+      content: _content,
+      contentPadding: widget.contentPadding,
+      contentTextStyle: widget.contentTextStyle,
+      actions: widget.actions,
+      actionsPadding: widget.actionsPadding,
+      actionsAlignment: widget.actionsAlignment,
+      actionsOverflowDirection: widget.actionsOverflowDirection,
+      actionsOverflowButtonSpacing: widget.actionsOverflowButtonSpacing,
+      buttonPadding: widget.buttonPadding,
+      backgroundColor: widget.backgroundColor,
+      elevation: widget.elevation,
+      semanticLabel: widget.semanticLabel,
+      insetPadding: widget.insetPadding,
+      clipBehavior: widget.clipBehavior,
+      shape: widget.shape,
+      scrollable: widget.scrollable,
     );
   }
 }
